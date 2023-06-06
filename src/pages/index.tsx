c02b528c77a66c5bd076c3a82a4dfffa76aae49e
jadsongmatos <jadson.g-matos@outlook.com>
2023-06-05 22:48:50 -0300

    façanha: Refatore o componente Chessboard e atualize as dependências
    - Melhore os nomes das variáveis de estado e a funcionalidade do tabuleiro de xadrez em index.tsx
    - Adicione botões e controle deslizante de intervalo para controlar o número de tópicos exibidos
    - Atualizar dependências em package.json
    - Atualize a imagem do MySQL e a senha do root em docker-compose.yml
    - Adicionar comando para plug-in de autenticação correto no contêiner MySQL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,24 @@ import { useCallback, useEffect, useRef, useState } from "react";
 const Chessground = dynamic(() => import("react-chessground"), { ssr: false });
 
 export default function Home() {
-  const [nThreads, setNThreads] = useState(1);
+  const [cpu_threads, set_cpu_threads] = useState(1);
+  const [n_threads, set_threads] = useState(1);
   useEffect(() => {
-    setNThreads(window.navigator.hardwareConcurrency);
+    set_cpu_threads(window.navigator.hardwareConcurrency);
   }, []);
+
+  const incrementar = () => {
+    if (n_threads < cpu_threads) {
+      set_threads(n_threads + 1);
+    }
+  };
+
+  const decrementar = () => {
+    if (n_threads > 1) {
+      set_threads(n_threads - 1);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -41,7 +55,138 @@ export default function Home() {
         <section className="container mb-5 text-center">
           <h1>Oracle Chess</h1>
         </section>
-        <section className="container my-5">{nThreads}</section>
+        <section className="container my-5">
+          <p>CPU: {cpu_threads} </p>
+          <p></p>
+        </section>
+        <section className="container">
+          <div className="row">
+            {n_threads > 1 ? (
+              <>
+                <div className="col-md-6">
+                  <ol className="list-group-numbered">
+                    {Array.from(
+                      { length: Math.ceil(n_threads / 2) },
+                      (v, i) => {
+                        return (
+                          <li key={i} className="mb-5">
+                            <div className="mx-auto">
+                              <Chessground
+                                fen={
+                                  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+                                }
+                                style={{
+                                  width: "200px",
+                                  height: "200px",
+                                  marginRight: "auto",
+                                  marginLeft: "auto",
+                                }}
+                                viewOnly={true}
+                                draggable={{ enabled: false }}
+                                addDimensionsCssVars={false}
+                              />
+                            </div>
+                          </li>
+                        );
+                      }
+                    )}
+                  </ol>
+                </div>
+                <div className="col-md-6">
+                  <ol className="list-group-numbered">
+                    {Array.from({ length: n_threads / 2 }, (v, i) => {
+                      return (
+                        <li key={i} className="mb-5">
+                          <div className="mx-auto">
+                            <Chessground
+                              fen={
+                                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+                              }
+                              style={{
+                                width: "200px",
+                                height: "200px",
+                                marginRight: "auto",
+                                marginLeft: "auto",
+                              }}
+                              viewOnly={true}
+                              draggable={{ enabled: false }}
+                              addDimensionsCssVars={false}
+                            />
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ol>
+                </div>
+              </>
+            ) : (
+              <div className="col-md-6">
+                <ol className="list-group-numbered">
+                  <li key={1} className="mb-5">
+                    <div className="mx-auto">
+                      <Chessground
+                        fen={
+                          "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+                        }
+                        style={{
+                          width: "200px",
+                          height: "200px",
+                          marginRight: "auto",
+                          marginLeft: "auto",
+                        }}
+                        viewOnly={true}
+                        draggable={{ enabled: false }}
+                        addDimensionsCssVars={false}
+                      />
+                    </div>
+                  </li>
+                </ol>
+              </div>
+            )}
+          </div>
+
+          <div
+            className="col-12 mt-5  btn-group btn-group-lg"
+            role="group"
+            aria-label="Large button group"
+          >
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={decrementar}
+            >
+              -
+            </button>
+            <button type="button" className="btn btn-outline-primary">
+              Threads: {n_threads}
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={incrementar}
+            >
+              +
+            </button>
+          </div>
+          <label htmlFor="rangeThread" className="form-label mt-3 ">
+            Example range
+          </label>
+          <input
+            type="range"
+            className="form-range col-12"
+            min={1}
+            max={cpu_threads}
+            step={1}
+            value={n_threads}
+            readOnly={false}
+            id="rangeThread"
+            onChange={(event) => {
+              const newValue = parseInt(event.target.value);
+              console.log("range", newValue);
+              set_threads(newValue);
+            }}
+          ></input>
+        </section>
       </main>
     </>
   );
