ffa6ac4e5a115f1340cd7e6e8af73ad2efa7b6a6
jadsongmatos <jadson.g-matos@outlook.com>
2023-06-21 23:23:57 -0300

    façanha: refatorar busca de dados e cache na página de perfil
    - Adicione três novas dependências ao package.json
    - Refatorar a busca de UF e dados da cidade na página de perfil
    - Adicionar cache para dados de UF e atualização dinâmica das opções de UF e cidade na página de perfil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,31 +6,53 @@ import "react-chessground/dist/styles/chessground.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
 import {
   QueryClient,
-  QueryClientProvider,
   Hydrate,
- // MutationCache,
+  //QueryClientProvider,
+  MutationCache,
 } from "@tanstack/react-query";
 
-// Create a client
-
+let persister: any = null;
+if (typeof window !== "undefined") {
+  persister = createSyncStoragePersister({
+    storage: window.localStorage,
+  });
+}
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = React.useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        cacheTime: 1000 * 60 * 60 * 24, // 24 hours
-        staleTime: 2000,
-        retry: 0,
-      },
-    },
-    //mutationCache: new MutationCache()
-  }));
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+            staleTime: 2000,
+            retry: 1,
+          },
+        },
+        mutationCache: new MutationCache(),
+      })
+  );
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+      onSuccess={() => {
+        // resume mutations after initial restore from localStorage was successful
+        queryClient.resumePausedMutations().then(() => {
+          queryClient.invalidateQueries();
+        });
+      }}
+    >
       <Hydrate state={pageProps.dehydratedState}>
         <Component {...pageProps} />
       </Hydrate>
-    </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen />
+    </PersistQueryClientProvider>
   );
 }
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -8,72 +8,6 @@ import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
 import Header from "@/components/header";
 import Select from "@/components/select";
 
-type Select_t = {
-  value: number;
-  label: String;
-};
-
-function buscaBinaria(array: Array<any>, alvo: number) {
-  let inicio = 0;
-  let fim = array.length - 1;
-
-  while (inicio <= fim) {
-    let meio = Math.floor((inicio + fim) / 2);
-
-    if (array[meio].value === alvo) {
-      // item encontrado, retorna o índice
-      return meio;
-    }
-    if (array[meio].value < alvo) {
-      // o alvo está à direita do meio, então move o início para meio + 1
-      inicio = meio + 1;
-    } else {
-      // o alvo está à esquerda do meio, então move o fim para meio - 1
-      fim = meio - 1;
-    }
-  }
-
-  // alvo não encontrado na lista
-  return -1;
-}
-
-const fetchDataUf = async (input: any) => {
-  //const index_counry = buscaBinaria(input.countries, input.counry.value);
-  console.log("fetchDataUf", input);
-
-  const res = await fetch(
-    `https://www.geonames.org/servlet/geonames?&srv=163&country=${input.counry.ISO}&featureCode=ADM1&lang=en&type=json`
-  );
-  if (!res.ok) {
-    throw new Error("Network response was not ok");
-  }
-  try {
-  } catch (error) {}
-  const uf_city = await res.json();
-  //console.log("uf_city", uf_city);
-  return uf_city.geonames.map((e: any) => {
-    return { value: Number(e.adminCode1), label: e.name };
-  });
-};
-
-const fetchDataCity = async (input: any) => {
-  console.log("fetchDataCity", input);
-
-  const res = await fetch(
-    `https://www.geonames.org/servlet/geonames?&srv=163&country=${input.counry.ISO}&adminCode1=${input.uf}&featureCode=ADM2&lang=en&type=json`
-  );
-  if (!res.ok) {
-    throw new Error("Network response was not ok");
-  }
-  try {
-  } catch (error) {}
-  const uf_city = await res.json();
-  //console.log("uf_city", uf_city);
-  return uf_city.geonames.map((e: any) => {
-    return { value: Number(e.adminCode1), label: e.name };
-  });
-};
-
 export default function ProfileEdit(props: any) {
   const queryClient = useQueryClient();
   const {
@@ -85,34 +19,110 @@ export default function ProfileEdit(props: any) {
   const [select_counry, set_select_counry] = useState(null);
   const [select_uf, set_select_uf] = useState(null);
   const [select_city, set_select_city] = useState(null);
+  const [uf_options, set_uf_options] = useState();
+  const [city_options, set_city_options] = useState();
 
-  const movieQuery = useQuery({
-    queryKey: ["uf_data"],
-    queryFn: () => fetchDataUf,
+  const counry_mutation = useMutation({
+    mutationKey: ["uf"],
+    mutationFn: async (input: any) => {
+      //const index_counry = buscaBinaria(input.countries, input.counry.value);
+      const previousData: any = queryClient.getQueryData([
+        "uf",
+        input.counry.value,
+      ]);
+
+      console.log("fetchDataUf", input, previousData);
+      if (previousData) {
+        set_uf_options(previousData);
+        return previousData;
+      }
+
+      const res = await fetch(
+        `https://www.geonames.org/servlet/geonames?&srv=163&country=${input.counry.ISO}&featureCode=ADM1&lang=en&type=json`
+      );
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const uf = await res.json();
+      const data = uf.geonames.map((e: any) => {
+        return { value: Number(e.adminCode1), label: e.name };
+      });
+      set_uf_options(data);
+      return data;
+    },
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({
+        queryKey: ["uf", variables.counry.value],
+      });
+      const previousData: any = queryClient.getQueryData([
+        "uf",
+        variables.counry.value,
+      ]);
+
+      //console.log("previousData", previousData, variables.counry.value);
+      return previousData?.data;
+    },
+    onSuccess: async (data: any, variables: any) => {
+      //console.log("useMutation", data, variables, select_counry);
+
+      queryClient.setQueryData(["uf", variables.counry.value], data);
+    },
+    /*onError: (_, variables, context) => {
+      queryClient.setQueryData(
+        ["uf_data", variables.counry.value],
+        context.previousData
+      );
+    },
+    onSettled: (data, error, variables) => {
+      queryClient.invalidateQueries(["uf_data", variables.counry.value]);
+    },*/
   });
 
-  const counry_options = useMutation({
-    onMutate: async () => {
-      await queryClient.cancelQueries({ queryKey: ["uf_data"] });
-      const previousData = queryClient.getQueryData(["uf_data"]);
-      console.log("previousData", previousData);
-      /*
-      // remove local state so that server state is taken instead
-      setComment(undefined)
+  const uf_mutation = useMutation({
+    mutationKey: ["city"],
+    mutationFn: async (input: any) => {
+      const previousData: any = queryClient.getQueryData([
+        "city",
+        input.counry.value,
+      ]);
 
-      queryClient.setQueryData(movieKeys.detail(movieId), {
-        ...previousData,
-        movie: {
-          ...previousData.movie,
-          comment,
-        },
-      })
+      if (previousData) {
+        set_city_options(previousData);
+        return previousData;
+      }
 
-      return { previousData }*/
+      const res = await fetch(
+        `https://www.geonames.org/servlet/geonames?&srv=163&country=${input.counry.ISO}&adminCode1=${input.uf}&featureCode=ADM2&lang=en&type=json`
+      );
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      try {
+        const city = await res.json();
+        const data = city.geonames.map((e: any) => {
+          return { value: Number(e.adminCode1), label: e.name };
+        });
+        set_city_options(data);
+        return data;
+      } catch (error) {
+        throw new Error("json");
+      }
+    },
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({
+        queryKey: ["city", variables.counry.value],
+      });
+      const previousData: any = queryClient.getQueryData([
+        "city",
+        variables.counry.value,
+      ]);
+      return previousData;
+    },
+    onSuccess: async (data: any, variables: any) => {
+      queryClient.setQueryData(["city", variables.counry.value], data);
     },
   });
-
-  const uf_options = useMutation(fetchDataCity);
 
   const onSubmit = (data: any) => console.log(data);
 
@@ -158,10 +168,13 @@ export default function ProfileEdit(props: any) {
                     options={props.countries}
                     onChange={(e: any) => {
                       set_select_counry(e);
-                      console.log("movieQuery",movieQuery)
-                      /*counry_options.mutate({
+                      set_select_uf(null);
+                      set_select_city(null);
+                      set_city_options(undefined)
+                      counry_mutation.mutate({
                         counry: e,
-                      });*/
+                      });
+
                       field.onChange(e); // make sure to keep this to update form state
                     }}
                     value={select_counry}
@@ -181,13 +194,13 @@ export default function ProfileEdit(props: any) {
               />
             </div>
             <div className="col-sm-6">
-              {counry_options.status == "loading" ? (
+              {counry_mutation.status == "loading" ? (
                 <div>Loading...</div>
               ) : (
                 <>
-                  {counry_options.status == "error" ? (
+                  {counry_mutation.status == "error" ? (
                     <div>
-                      An error has occurred: {JSON.stringify(counry_options)}
+                      An error has occurred: {JSON.stringify(counry_mutation)}
                     </div>
                   ) : (
                     <Controller
@@ -197,10 +210,11 @@ export default function ProfileEdit(props: any) {
                       render={({ field }) => (
                         <Select
                           {...field}
-                          options={counry_options.data}
+                          options={uf_options}
                           onChange={(e: any) => {
                             set_select_uf(e);
-                            uf_options.mutate({
+                            set_select_city(null);
+                            uf_mutation.mutate({
                               counry: select_counry,
                               uf: e.value,
                             });
@@ -226,13 +240,13 @@ export default function ProfileEdit(props: any) {
               )}
             </div>
             <div className="col-sm-6">
-              {uf_options.status == "loading" ? (
+              {uf_mutation.status == "loading" ? (
                 <div>Loading...</div>
               ) : (
                 <>
-                  {uf_options.status == "error" ? (
+                  {uf_mutation.status == "error" ? (
                     <div>
-                      An error has occurred: {JSON.stringify(uf_options)}
+                      An error has occurred: {JSON.stringify(uf_mutation)}
                     </div>
                   ) : (
                     <Controller
@@ -242,7 +256,7 @@ export default function ProfileEdit(props: any) {
                       render={({ field }) => (
                         <Select
                           {...field}
-                          options={uf_options.data}
+                          options={city_options}
                           onChange={(e: any) => {
                             set_select_city(e);
                             field.onChange(e);
@@ -266,7 +280,6 @@ export default function ProfileEdit(props: any) {
                 </>
               )}
             </div>
-
             <button className="btn btn-primary col-md-4" type="submit">
               Update Profile
             </button>
